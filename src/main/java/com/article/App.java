487fdbf44e6a1d45e2ecd950c743ca689cecc6de
add <a992361951@gmail.com>
2025-03-03 15:23:14 +0800

    check the args

--- src/main/java/com/article/App.java
+++ src/main/java/com/article/App.java
@@ -1,10 +1,17 @@
 package com.article;
 
+import com.huaban.analysis.jieba.JiebaSegmenter;
+import com.huaban.analysis.jieba.SegToken;
 /**
  * Hello world!
  */
 public class App {
+//first step : check the args
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length != 3) {
+            System.out.println("用法: java -jar checker.jar <原文路径> <抄袭版路径> <输出路径>");
+            return;
+        }
     }
 }
